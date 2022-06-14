e186aba575c0f7bec50ced2a5707a71db0412eb0
Vinay Verma <vinayverma610@gmail.com>
2022-06-14 14:09:12 +0530

    Add main consumer class

---
+++ src/main/java/LogConsumer.java
@@ -0,0 +1,46 @@
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class LogConsumer {
+    private static final Logger log = LoggerFactory.getLogger(LogConsumer.class.getSimpleName());
+
+    public static void main(String[] args) throws InterruptedException {
+        String bootstrapServer = "127.0.0.1:9092";
+        String group_id = "consumer-group-1";
+        String topic = "topic1";
+
+        //create consumer properties
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, group_id);
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        //create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        //subscribe consumer to topic(s)
+        consumer.subscribe(Collections.singletonList(topic));
+
+        while(true) {
+            log.info("Polling...");
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for(ConsumerRecord<String, String> record : records) {
+                log.info("key:" + record.key() + ", value:" + record.value());
+            }
+            Thread.sleep(1000);
+        }
+
+    }
+}
